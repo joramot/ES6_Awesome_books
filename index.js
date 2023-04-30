74203c5b1231a91b90ab2bd6854fe4bf0ba2cbdd
JOSE RAUL MOJICA <mojica.raul@gmail.com>
2023-04-30 15:57:13 -0600

    Delete some code lines in the index.js file

--- index.js
+++ index.js
@@ -6,28 +6,9 @@ const bookList = new BookList();
 const navList = document.querySelector('.menuList');
 const navAdd = document.querySelector('.menuAdd');
 const navContact = document.querySelector('.menuContact');
-// const addBooks = document.querySelector('.add-book');
-// const listBooks = document.querySelector('.list');
-// const contactInfo = document.querySelector('.contact');
 
 navAdd.addEventListener('click', handleNavAddClick);
 navList.addEventListener('click', handleNavListClick);
 navContact.addEventListener('click', handleNavContactClick);
-
-// navAdd.addEventListener('click', () => {
-//   addBooks.style.display = 'flex';
-//   listBooks.style.display = 'none';
-//   contactInfo.style.display = 'none';
-// });
-
-// navList.addEventListener('click', () => {
-//   window.location.reload();
-// });
-
-// navContact.addEventListener('click', () => {
-//   contactInfo.style.display = 'flex';
-//   listBooks.style.display = 'none';
-//   addBooks.style.display = 'none';
-// });
 
 bookList.init();

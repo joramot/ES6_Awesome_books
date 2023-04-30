5d0d01cfda084429519db54658651f17fc39f2e9
JOSE RAUL MOJICA <mojica.raul@gmail.com>
2023-04-30 15:37:16 -0600

    Add import statement in index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+import BookList from './modules/bookList.js';
+
+const bookList = new BookList();
+
+const navList = document.querySelector('.menuList');
+const navAdd = document.querySelector('.menuAdd');
+const navContact = document.querySelector('.menuContact');
+const addBooks = document.querySelector('.add-book');
+const listBooks = document.querySelector('.list');
+const contactInfo = document.querySelector('.contact');
+
+navAdd.addEventListener('click', () => {
+  addBooks.style.display = 'flex';
+  listBooks.style.display = 'none';
+  contactInfo.style.display = 'none';
+});
+
+navList.addEventListener('click', () => {
+  window.location.reload();
+});
+
+navContact.addEventListener('click', () => {
+  contactInfo.style.display = 'flex';
+  listBooks.style.display = 'none';
+  addBooks.style.display = 'none';
+});
+
+bookList.init();

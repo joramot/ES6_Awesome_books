2f5b19096091cd83583fc94ad373084e516b52eb
JOSE RAUL MOJICA <mojica.raul@gmail.com>
2023-04-30 15:53:41 -0600

    Add import statement for navMenu.js

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
 import BookList from './modules/bookList.js';
+import { handleNavAddClick, handleNavListClick, handleNavContactClick } from './modules/navMenu.js';
 
 const bookList = new BookList();
 
 const navList = document.querySelector('.menuList');
 const navAdd = document.querySelector('.menuAdd');
 const navContact = document.querySelector('.menuContact');
-const addBooks = document.querySelector('.add-book');
-const listBooks = document.querySelector('.list');
-const contactInfo = document.querySelector('.contact');
+// const addBooks = document.querySelector('.add-book');
+// const listBooks = document.querySelector('.list');
+// const contactInfo = document.querySelector('.contact');
 
-navAdd.addEventListener('click', () => {
-  addBooks.style.display = 'flex';
-  listBooks.style.display = 'none';
-  contactInfo.style.display = 'none';
-});
+navAdd.addEventListener('click', handleNavAddClick);
+navList.addEventListener('click', handleNavListClick);
+navContact.addEventListener('click', handleNavContactClick);
 
-navList.addEventListener('click', () => {
-  window.location.reload();
-});
+// navAdd.addEventListener('click', () => {
+//   addBooks.style.display = 'flex';
+//   listBooks.style.display = 'none';
+//   contactInfo.style.display = 'none';
+// });
 
-navContact.addEventListener('click', () => {
-  contactInfo.style.display = 'flex';
-  listBooks.style.display = 'none';
-  addBooks.style.display = 'none';
-});
+// navList.addEventListener('click', () => {
+//   window.location.reload();
+// });
+
+// navContact.addEventListener('click', () => {
+//   contactInfo.style.display = 'flex';
+//   listBooks.style.display = 'none';
+//   addBooks.style.display = 'none';
+// });
 
 bookList.init();

a924cd6e38875f0332fee3adc8399a61be8d649d
JOSE RAUL MOJICA <mojica.raul@gmail.com>
2023-05-01 09:40:15 -0600

    Install luxon library, create luxon.js file, add datetime line in the index.html, import luxon.js in index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import { DateTime } from './modules/luxon.js';
 import BookList from './modules/bookList.js';
 import { handleNavAddClick, handleNavListClick, handleNavContactClick } from './modules/navMenu.js';
 
@@ -6,6 +7,12 @@ const bookList = new BookList();
 const navList = document.querySelector('.menuList');
 const navAdd = document.querySelector('.menuAdd');
 const navContact = document.querySelector('.menuContact');
+
+const date = document.querySelector('.date');
+setInterval(() => {
+  const now = DateTime.now();
+  date.textContent = now.toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
+}, 1000);
 
 navAdd.addEventListener('click', handleNavAddClick);
 navList.addEventListener('click', handleNavListClick);
